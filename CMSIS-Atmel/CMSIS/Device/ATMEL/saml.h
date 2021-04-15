85ee9291484343e6da8f5e81ec420bdb943e09c7
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2021-04-15 11:45:17 +0700

    Fix SAML core includes

--- CMSIS-Atmel/CMSIS/Device/ATMEL/saml.h
+++ CMSIS-Atmel/CMSIS/Device/ATMEL/saml.h
@@ -21,34 +21,34 @@
 #define _SAML_INCLUDED_
 
 #if   defined(__SAML21E18A__) || defined(__ATSAML21E18A__)
-  #include "saml21a1/saml21e18a.h"
+  #include "saml21a1/include/saml21e18a.h"
 #elif defined(__SAML21G18A__) || defined(__ATSAML21G18A__)
-  #include "saml21a1/saml21g18a.h"
+  #include "saml21a1/include/saml21g18a.h"
 #elif defined(__SAML21J18A__) || defined(__ATSAML21J18A__)
-  #include "saml21a1/saml21j18a.h"
+  #include "saml21a1/include/saml21j18a.h"
 
 #elif defined(__SAML21E15B__) || defined(__ATSAML21E15B__)
-  #include "saml21b/saml21e15b.h"
+  #include "saml21b/include/saml21e15b.h"
 #elif defined(__SAML21E16B__) || defined(__ATSAML21E16B__)
-  #include "saml21b/saml21e16b.h"
+  #include "saml21b/include/saml21e16b.h"
 #elif defined(__SAML21E17B__) || defined(__ATSAML21E17B__)
-  #include "saml21b/saml21e17b.h"
+  #include "saml21b/include/saml21e17b.h"
 #elif defined(__SAML21E18B__) || defined(__ATSAML21E18B__)
-  #include "saml21b/saml21e18b.h"
+  #include "saml21b/include/saml21e18b.h"
 #elif defined(__SAML21G16B__) || defined(__ATSAML21G16B__)
-  #include "saml21b/saml21g16b.h"
+  #include "saml21b/include/saml21g16b.h"
 #elif defined(__SAML21G17B__) || defined(__ATSAML21G17B__)
-  #include "saml21b/saml21g17b.h"
+  #include "saml21b/include/saml21g17b.h"
 #elif defined(__SAML21G18B__) || defined(__ATSAML21G18B__)
-  #include "saml21b/saml21g18b.h"
+  #include "saml21b/include/saml21g18b.h"
 #elif defined(__SAML21J16B__) || defined(__ATSAML21J16B__)
-  #include "saml21b/saml21j16b.h"
+  #include "saml21b/include/saml21j16b.h"
 #elif defined(__SAML21J17B__) || defined(__ATSAML21J17B__)
-  #include "saml21b/saml21j17b.h"
+  #include "saml21b/include/saml21j17b.h"
 #elif defined(__SAML21J18B__) || defined(__ATSAML21J18B__)
-  #include "saml21b/saml21j18b.h"
+  #include "saml21b/include/saml21j18b.h"
 #elif defined(__SAML21J18BU__) || defined(__ATSAML21J18BU__)
-  #include "saml21b/saml21j18bu.h"
+  #include "saml21b/include/saml21j18bu.h"
 #else
   #error Device not supported
 #endif
